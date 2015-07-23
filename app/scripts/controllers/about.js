1bfdcac5202034ca79c7aed39e70307f103af768
scorpio-v <scorpio-v@users.noreply.github.com>
2015-07-23 19:00:21 +0530

    Create about.js

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name incidentManagerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the incidentManagerApp
+ */
+angular.module('incidentManagerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
