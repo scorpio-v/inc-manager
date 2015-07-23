79556d8d378540e719d815af5c7bba6de3ad4a2a
scorpio-v <scorpio-v@users.noreply.github.com>
2015-07-23 19:02:36 +0530

    Create incidentservice.js

---
+++ app/scripts/services/incidentservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name incidentManagerApp.incidentService
+ * @description
+ * # incidentService
+ * Service in the incidentManagerApp.
+ */
+angular.module('incidentManagerApp')
+  .service('incidentService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
