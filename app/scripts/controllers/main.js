2eefa42baeb5d8577d3c6328abafec74a4cde1c3
scorpio-v <scorpio-v@users.noreply.github.com>
2015-07-23 19:00:06 +0530

    Create main.js

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name incidentManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the incidentManagerApp
+ */
+angular.module('incidentManagerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
