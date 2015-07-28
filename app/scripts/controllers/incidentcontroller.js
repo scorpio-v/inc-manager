196cd84576bff26b75bb2248c423d75b112d2330
scorpio-v <scorpio-v@users.noreply.github.com>
2015-07-28 15:16:21 +0530

    Update incidentcontroller.js

--- app/scripts/controllers/incidentcontroller.js
+++ app/scripts/controllers/incidentcontroller.js
@@ -7,8 +7,25 @@
  * # IncidentcontrollerCtrl
  * Controller of the incidentManagerApp
  */
-var incidentForm = angular.module('incidentManagerApp')
-  .controller('IncidentcontrollerCtrl', function ($scope) {
-      // we will store our form data in this object
-      $scope.formData = {};
-  });
+incidentManagerApp.controller('IncidentcontrollerCtrl', ['CreateIncidentService', '$scope', function (CreateIncidentService, scope) {
+  	scope.query='';
+  	scope.users = '';
+  	scope.company = '';
+  	console.log('scope.query : ' + scope.query);
+
+  	scope.getAffectedUsers = function() {
+  		console.log("Getting Affected Users...");
+  		  CreateIncidentService.getAffectedUsers(scope.query).then(function(user){
+  			  console.log('Controller : ' + user);
+  			  scope.users = user;
+  		});
+  	};
+
+  	scope.getAffectedCompany = function() {
+  		console.log("Getting Affected Users...");
+  		  CreateIncidentService.getAffectedCompany(scope.query).then(function(company){
+  			  console.log('Controller : ' + company);
+  			  scope.company = company;
+  		});
+  	};
+}]);
